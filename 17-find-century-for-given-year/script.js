88adfc6f7d22da963143dea8c7891a248e94cc8b
Niraj Pradhan <niraj.pradhan.393@gmail.com>
2023-02-12 18:14:12 +0545

    code: refactor

--- 17-find-century-for-given-year/script.js
+++ 17-find-century-for-given-year/script.js
@@ -1,8 +1,4 @@
-const GetCentury = (year) => {
-  if (year - 100 <= 0) return 1;
-  else if (year % 100 === 0) return year / 100;
-  else return Math.ceil(year / 100); // or you can use Math.floor(year) + 1
-};
+const GetCentury = (year) => Math.ceil(year / 100);
 
 console.log(GetCentury(19)); // 1
 console.log(GetCentury(1900)); // 19
